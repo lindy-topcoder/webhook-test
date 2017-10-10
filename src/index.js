eb3eea0e7c0d4620528755f19170227b38846bdf
Mike Lindenmuth <mlindenmuth@topcoder.com>
2017-10-10 00:28:28 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import expressWs from 'express-ws';
+
+const app = express();
+const wss = expressWs(app).getWss();
+
+const config = [
+  {
+    "repository": "git@github.com:test/some-private-repo.git",
+    "branch": "master",
+    "path": "path/to/swagger.yml"
+  },
+]
+
+
+
+app.post('/api/webhooks/github', express.json(), (request, response) => {
+  response.send(request.body);
+})
+
+app.ws('/ws', (ws, request) => {
+
+});
+
+app.use((error, request, response, next) => {
+  response.status(error.status);
+  response.json({message: error.message})
+})
+
+app.listen(process.env.PORT || 5000);
